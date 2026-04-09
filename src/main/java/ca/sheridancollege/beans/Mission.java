2f662c0d149ef26d8dbe96eeed566b32a479b2df
bapaksukareview <bapaksukareview@gmail.com>
2026-04-09 11:47:52 +0700

    Update Mission.java

--- src/main/java/ca/sheridancollege/beans/Mission.java
+++ src/main/java/ca/sheridancollege/beans/Mission.java
@@ -8,31 +8,56 @@ import lombok.Data;
  * @author Mihye
  */
 @Data
+
 public class Mission {
-	private Long id;
-	private String agent;
-	private String title;
-	private String gadget1;
-	private String gadget2;
 
-public Long getId() {
-    return id;
-}
+    private Long id;
+    private String agent;
+    private String title;
+    private String gadget1;
+    private String gadget2;
 
-public String getAgent() {
-    return agent;
-}
+    // ===== GETTERS =====
+    public Long getId() {
+        return id;
+    }
 
-public String getTitle() {
-    return title;
-}
+    public String getAgent() {
+        return agent;
+    }
 
-public String getGadget1() {
-    return gadget1;
-}
+    public String getTitle() {
+        return title;
+    }
 
-public String getGadget2() {
-    return gadget2;
+    public String getGadget1() {
+        return gadget1;
+    }
+
+    public String getGadget2() {
+        return gadget2;
+    }
+
+    // ===== SETTERS =====
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAgent(String agent) {
+        this.agent = agent;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setGadget1(String gadget1) {
+        this.gadget1 = gadget1;
+    }
+
+    public void setGadget2(String gadget2) {
+        this.gadget2 = gadget2;
+    }
 }
 	
 	// constructor with fields for later use in the test
